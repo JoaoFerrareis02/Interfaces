c3fc8dc53c9fd1de9196021b8f2668b2c6ade808
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2023-01-02 11:46:56 -0300

    Progama realizado com uma Interface (escrito em inglês)

--- src/model/services/BrazilTaxService.java
+++ src/model/services/BrazilTaxService.java
@@ -1,6 +1,6 @@
 package model.services;
 
-public class BrazilTaxService {
+public class BrazilTaxService implements TaxService{
 
 	public double tax(double ammount) {
 		return ammount <= 100.00 ? ammount * 0.2 : ammount * 0.15;
